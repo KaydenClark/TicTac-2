d3ff1a971b939ddf7d43b0d034f0dfef51f9ed5b
Kayden Clark <kaydenclark725@gmail.com>
2020-02-24 05:43:22 -0700

    push to git

---
+++ src/components/square.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../style.css'
+
+export default class Square extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+          value: null,
+        };
+    }
+
+    render(){
+        return(
+            <button 
+            className= "square" 
+            onClick= {() => {
+                this.props.onClick()
+                }}
+            >
+                {this.props.value}
+            </button>
+        )
+    }
+}
